63fb83b946203d0f53ed9dc755f3bd1be0aaad55
huidong <2804234243@qq.com>
2020-08-20 23:50:47 +0800

    COMMIT:配置eureka

---
+++ cloud-eureka-server7001/src/main/java/pers/huidong/springcloud/EurekaMain7001.java
@@ -0,0 +1,19 @@
+package pers.huidong.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author: HuiDong XU
+ * @Description:
+ * @Date: 2020/8/20 23:28
+ * @Version: 1.0
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class,args);
+    }
+}
